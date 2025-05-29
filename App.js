69762ba8fdc5596c668c2320327320daf8f625c8
Faahim15 <abdulkaderfahim6@gmail.com>
2025-05-30 04:54:18 +0600

    Meal details page added

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import "./global.css";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
-
+import DetailsMealScreen from "./screens/DetailsMealScreen";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverView" component={MealsOverviewScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            options={{
+              title: "All Categories",
+            }}
+            name="MealsCategories"
+            component={CategoriesScreen}
+          />
+          <Stack.Screen
+            name="MealsOverView"
+            component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId;
+            //   return {
+            //     title: catId,
+            //   };
+            // }}
+          />
+          <Stack.Screen name="MealDetails" component={DetailsMealScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,20 @@
 import { View, Text, FlatList } from "react-native";
-import React from "react";
-import { MEALS } from "../data/dummyData";
+import React, { useEffect, useLayoutEffect } from "react";
+import { MEALS, CATEGORIES } from "../data/dummyData";
 import MealItem from "../components/MealItem";
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ navigation, route }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId);
+    if (categoryTitle) navigation.setOptions({ title: categoryTitle.title });
+  }, [catId, navigation]);
+
   function renderMealItem({ item }) {
     const mealItemProps = {
       title: item.title,
@@ -16,6 +22,7 @@ export default function MealsOverviewScreen({ route }) {
       affordability: item.affordability,
       complexity: item.complexity,
       duration: item.duration,
+      id: item.id,
     };
     return <MealItem {...mealItemProps}></MealItem>;
   }
